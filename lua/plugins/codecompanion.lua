4d3578217573fdfde9ec650c7ec0e3440aee64fa
osman.soeguet <osman.soeguet@convotis.com>
2025-07-02 14:28:30 +0200

    add: fidget

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,8 +1,18 @@
 return {
-  "olimorris/codecompanion.nvim",
-  opts = {},
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function(_, opts)
+        require("codecompanion").setup(opts)
+
+        vim.keymap.set({ "n", "v" }, "<leader>aa", "<cmd>CodeCompanionActions<cr>", { noremap = true })
+        vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+        vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+        -- Expand 'cc' into 'CodeCompanion' in the command line
+        vim.cmd([[cab cc CodeCompanion]])
+    end,
 }
