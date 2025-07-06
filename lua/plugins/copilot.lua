955394d625e4f74ea84e0eff55cfc8c87e23cd01
soeguet <soeguet.osman@gmail.com>
2025-07-06 22:46:52 +0200

    updte

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,13 +6,13 @@ return {
 		require("copilot").setup({
 			suggestion = {
 				enabled = true,
-				auto_trigger = true,
+				auto_trigger = false,
 				hide_during_completion = true,
 				debounce = 75,
 				trigger_on_accept = true,
 				keymap = {
 					accept = "<TAB>",
-					accept_word = false,
+					accept_word = "<C-l>",
 					accept_line = false,
 					next = "<M-]>",
 					prev = "<M-[>",
@@ -23,9 +23,9 @@ return {
 
         vim.api.nvim_set_hl(0, "CopilotSuggestion", {
             fg = "#ff6961",
-            bg = "#000000",
+            -- bg = "#000000",
             blend = 30,
-            undercurl = true,  -- Subtile Unterstreichung
+            undercurl = false,  -- Subtile Unterstreichung
             sp = "#665c54"     -- Farbe der Unterstreichung
 
         })
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
     { 'echasnovski/mini.icons', version = '*', opts = {} },
     { 'echasnovski/mini.statusline', version = '*', opts = {}  },
     { 'echasnovski/mini.splitjoin', version = '*', opts = {} },
-    { 'echasnovski/mini.diff', version = '*', opts = {} },
+    -- { 'echasnovski/mini.diff', version = '*', opts = {} },
     -- { 'echasnovski/mini.clue', version = '*' },
-    { 'echasnovski/mini.notify', enabled = false, version = '*', opts = {} },
+    -- { 'echasnovski/mini.notify', version = '*', opts = {} },
 }
